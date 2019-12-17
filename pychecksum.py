ccd1b0a0ecac0ab4a16e4538bee1933417141fd4
Kien Tran <kientrancm@gmail.com>
2019-12-17 11:35:39 +0700

    Finished implemented, first release!

--- pychecksum.py
+++ pychecksum.py
@@ -1,5 +1,6 @@
 #Author     Date            Version     Descriptions
 #KTR        Dec 16 2019     1.0         Init document
+#This script will be help you for get the md5 string. You just select an file.
 
 import subprocess
 import os
@@ -8,17 +9,6 @@ from tkinter import *
 from tkinter import filedialog
 
 import hashlib
-
-
-# This functions will be used for get the Md5 string from file.
-def CheckMd5(inputFile):
-    with open(inputFile, "rb") as f:
-        file_hash = hashlib.md5()
-        while chunk := f.read(8192):
-            file_hash.update(chunk)
-
-    return (file_hash.hexdigest())
-
 
 #GUI
 class GUI(tkinter.Frame):
@@ -29,7 +19,7 @@ class GUI(tkinter.Frame):
         self.initGui()
 
     def initMenu(self):
-        self.filepath = ""
+        self.filepaths = []
         self.root.title("Get Md5 with Python - Pychecksum | version 1.0")
         self.pack(fill=BOTH, expand=1)
 
@@ -59,38 +49,53 @@ class GUI(tkinter.Frame):
         self.LeftBotLabelFrame.grid(row=2, column=1, sticky = W)
         self.RightBotLabelFrame.grid(row=2, column=2, sticky = W)
         
+        self.MD5 = tkinter.StringVar()
 
         self.PathValue = tkinter.StringVar()
         self.statusValue = StringVar()
         self.statusValue.set('Please select your files')
-        self.NameVar = tkinter.StringVar()
+        self.MD5 = tkinter.StringVar()
 
         self.InputGroupLabel = Label(self.inputFrame, text = "-----> Input files <-----", width = 60).grid(row = 1, column = 1, columnspan = 2)
-
+        self.ChooserButton = Button(self.inputFrame, text='Input', command=self.OpenFile).grid(row=2, column=1)
+        self.PathEntry = Entry(self.inputFrame, width=60, bd=2, textvariable=self.PathValue).grid(row=2, column=2)
+        self.Md5Label = Label(self.inputFrame, text = 'MD5').grid(row = 3, column = 1)
+        self.Md5Entry = Entry(self.inputFrame, width = 60, bd = 2, textvariable = self.MD5).grid(row = 3, column = 2)
 
         self.RunGroupLabel = Label(self.LeftBotLabelFrame, text='-----> Run Automation Scripts <-----', width = 60).grid(row=1, column=1, columnspan = 2)
         self.generateButton = Button(self.LeftBotLabelFrame, text='Generate', command=self.myGUI).grid(row=2, column=1, columnspan=2)
         self.statusLabel = Label(self.LeftBotLabelFrame, textvariable=self.statusValue).grid(row=3, column=1, columnspan=2)
 
 
-
-
-
-
-
     def OpenFile(self):
         file_path = tkinter.filedialog.askopenfilename(filetype = (("PDF", "*.pdf"), ("All files", "*.*")), parent=self,)
-        self.file_path = file_path
-        self.PathValue.set(self.file_path)
+        self.file_paths = file_path
+        self.PathValue.set(self.file_paths)
+
+    
+    # This functions will be used for get the Md5 string from file.
+    def CheckMd5(self, inputFile):
+        with open(inputFile, "rb") as f:
+            file_hash = hashlib.md5()
+            while chunk := f.read(8192):
+                file_hash.update(chunk)
+
+        return (file_hash.hexdigest())
+
+    def setStatus(self, status):
+        self.statusValue.set(status)
 
     def myGUI(self):
-        print("Jueb")
+        if not (self.PathValue.get()):
+            self.setStatus('Missing input file')
+        else:
+            self.setStatus('Get MD5...')
+            file = self.file_paths
+            #name = os.path.splitext(file)[0]
+            #name = os.path.basename(name)
+            self.MD5.set(self.CheckMd5(file))
+            self.setStatus('Finished! Get your md5')
         
-
-
-
-
-
 def runGUI():
     root = tkinter.Tk()
     runGUI = GUI(root)
